56623fc2a0aa7eb6004570e2f4f3de54c1cd4fd0
Rafael de Sa <rafael_jdsb@hotmail.com>
2018-04-15 10:42:42 -0300

    Sistema Financeiro Pessoal

---
+++ src/main/java/br/com/javaparaweb/financeiro/util/DAOFactory.java
@@ -0,0 +1,14 @@
+package br.com.javaparaweb.financeiro.util;
+
+import br.com.javaparaweb.financeiro.usuario.UsuarioDAO;
+import br.com.javaparaweb.financeiro.usuario.UsuarioDAOHibernate;
+
+public class DAOFactory {
+	
+	public static UsuarioDAO criarUsuarioDAO() {
+		UsuarioDAOHibernate usuarioDAO = new UsuarioDAOHibernate();
+		usuarioDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+		return usuarioDAO;
+	}
+	
+}
