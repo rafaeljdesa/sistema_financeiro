690c8f3f2d7cb24454e4655cb9dbd4d7b8f6dc39
Rafael de Sa <rafael_jdsb@hotmail.com>
2018-04-16 21:51:30 -0300

    Sistema Financeiro Pessoal

--- src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
+++ src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
@@ -1,5 +1,7 @@
 package br.com.javaparaweb.financeiro.web;
 
+import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -13,8 +15,11 @@ import br.com.javaparaweb.financeiro.usuario.UsuarioRN;
 public class UsuarioBean {
 	private Usuario usuario = new Usuario();
 	private String confirmarSenha;
+	private List<Usuario> lista;
+	private String destinoSalvar;
 	
 	public String novo() {
+		this.destinoSalvar = "usuarioSucesso";
 		this.usuario = new Usuario();
 		this.usuario.setAtivo(true);
 		return "/publico/usuario";
@@ -33,7 +38,40 @@ public class UsuarioBean {
 		UsuarioRN usuarioRN = new UsuarioRN();
 		usuarioRN.salvar(this.usuario);
 		
-		return "usuariosucesso";
+		return this.destinoSalvar;
+	}
+	
+	public String editar() {
+		this.confirmarSenha = this.usuario.getSenha();
+		return "/publico/usuario";
+	}
+	
+	public String excluir() {
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.excluir(this.usuario);
+		this.lista = null;
+		return null;
+	}
+	
+	public String ativar() {
+		if(this.usuario.isAtivo()) {
+			this.usuario.setAtivo(false);
+		}else {
+			this.usuario.setAtivo(true);
+		}
+		
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+		return null;
+	}
+	
+	public List<Usuario> getLista(){
+		if(this.lista == null) {
+			UsuarioRN usuarioRN = new UsuarioRN();
+			this.lista = usuarioRN.listar();
+		}
+		
+		return this.lista;
 	}
 	
 	public Usuario getUsuario() {
@@ -47,6 +85,18 @@ public class UsuarioBean {
 	}
 	public void setConfirmarSenha(String confirmarSenha) {
 		this.confirmarSenha = confirmarSenha;
+	}
+
+	public String getDestinoSalvar() {
+		return destinoSalvar;
+	}
+
+	public void setDestinoSalvar(String destinoSalvar) {
+		this.destinoSalvar = destinoSalvar;
+	}
+
+	public void setLista(List<Usuario> lista) {
+		this.lista = lista;
 	}
 	
 	
